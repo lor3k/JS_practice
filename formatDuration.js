c33e223a0f756bb5c1ecbd74eb616a27c0174e01
Pawel <pawel@MacBook-Pro-Pawel.local>
2017-12-16 14:38:37 +0100

    first commit

---
+++ formatDuration.js
@@ -0,0 +1,9 @@
+/*
+ * To jest brudnopis JavaScriptu.
+ *
+ * Wprowadź kod JavaScript, następnie kliknij prawym przyciskiem lub wybierz z menu Wykonaj:
+ * 1. Uruchom – by wyliczyć zaznaczone wyrażenie (Cmd-R)
+ * 2. Zbadaj – by wyświetlić wynik w Inspektorze obiektów (Cmd-I)
+ * 3. Wyświetl – by wstawić wynik w komentarzu za zaznaczeniem. (Cmd-L)
+ */
+
